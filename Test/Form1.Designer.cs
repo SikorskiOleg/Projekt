b9f7ff6c32008f4b72881b9e0bcc5a48b9624fd5
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-06-18 19:09:01 +0200

    Gotowa wypowiedz sfinksa

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tytul = new System.Windows.Forms.Label();
+            this.wstep = new System.Windows.Forms.TextBox();
+            this.smierc = new System.Windows.Forms.Button();
+            this.wysluchaj = new System.Windows.Forms.Button();
+            this.uciekaj = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tytul
+            // 
+            this.tytul.AutoSize = true;
+            this.tytul.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tytul.Location = new System.Drawing.Point(378, 37);
+            this.tytul.Name = "tytul";
+            this.tytul.Size = new System.Drawing.Size(483, 90);
+            this.tytul.TabIndex = 0;
+            this.tytul.Text = "Próba Sfinxa";
+            // 
+            // wstep
+            // 
+            this.wstep.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.wstep.Location = new System.Drawing.Point(60, 210);
+            this.wstep.Multiline = true;
+            this.wstep.Name = "wstep";
+            this.wstep.Size = new System.Drawing.Size(1146, 289);
+            this.wstep.TabIndex = 1;
+            this.wstep.TextChanged += new System.EventHandler(this.wstep_TextChanged);
+            // 
+            // smierc
+            // 
+            this.smierc.Location = new System.Drawing.Point(564, 556);
+            this.smierc.Name = "smierc";
+            this.smierc.Size = new System.Drawing.Size(180, 85);
+            this.smierc.TabIndex = 2;
+            this.smierc.Text = "Obejdź Sfinxa";
+            this.smierc.UseVisualStyleBackColor = true;
+            // 
+            // wysluchaj
+            // 
+            this.wysluchaj.Location = new System.Drawing.Point(825, 556);
+            this.wysluchaj.Name = "wysluchaj";
+            this.wysluchaj.Size = new System.Drawing.Size(198, 85);
+            this.wysluchaj.TabIndex = 3;
+            this.wysluchaj.Text = "Wysłuchaj Zagadek";
+            this.wysluchaj.UseVisualStyleBackColor = true;
+            // 
+            // uciekaj
+            // 
+            this.uciekaj.Location = new System.Drawing.Point(290, 556);
+            this.uciekaj.Name = "uciekaj";
+            this.uciekaj.Size = new System.Drawing.Size(180, 85);
+            this.uciekaj.TabIndex = 4;
+            this.uciekaj.Text = "Uciekaj";
+            this.uciekaj.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1260, 690);
+            this.Controls.Add(this.uciekaj);
+            this.Controls.Add(this.wysluchaj);
+            this.Controls.Add(this.smierc);
+            this.Controls.Add(this.wstep);
+            this.Controls.Add(this.tytul);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label tytul;
+        private System.Windows.Forms.TextBox wstep;
+        private System.Windows.Forms.Button smierc;
+        private System.Windows.Forms.Button wysluchaj;
+        private System.Windows.Forms.Button uciekaj;
     }
 }
 
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -16,5 +16,13 @@ namespace Test
         {
             InitializeComponent();
         }
+
+        private void wstep_TextChanged(object sender, EventArgs e)
+        {
+            wstep.Text = "Drzwi grobowca z wolna ustępują pod Twoim naciskiem, skrzypiąc jak kości starożytnego strażnika. Powietrze jest gęste od pyłu i woni zbutwiałych tkanin – to miejsce nie widziało żywego oddechu od stuleci. Pochodnia w Twojej dłoni rzuca niespokojne blaski, a cienie na ścianach zdają się drżeć wbrew prawom fizyki. Nagle, kilka kroków przed Tobą, mrok się rozstępuje..." + " " +
+                "\r\n\r\n► Przed Tobą stoi Sfinks.\r\n\r\nJego kamienne ciało, pokryte pęknięciami czasu, zdaje się żyć w migotliwym świetle płomienia. Oczy – głębokie jak studnie prowadzące do podziemi – wpatrują się w Ciebie z nieludzką cierpliwością. Kiedy otwierasz usta, by przemówić, wargi Sfinksa poruszają się pierwsze, a głos rozlega się nie w uszach, lecz w Twoich kościach:" + " " +
+                "\r\n\r\n\"Witaj, śmiertelniku. Czy jesteś gotów stanąć przed Sędzią Zagubionych Dusz?\""
+
+        }
     }
 }
